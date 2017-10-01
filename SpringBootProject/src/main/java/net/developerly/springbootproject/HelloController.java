755bb67803b6263537236e1c426316c8c0507e9d
LY <coderyliu@gmail.com>
2017-10-01 15:28:27 +0800

    配置文件的读取

--- SpringBootProject/src/main/java/net/developerly/springbootproject/HelloController.java
+++ SpringBootProject/src/main/java/net/developerly/springbootproject/HelloController.java
@@ -1,5 +1,7 @@
 package net.developerly.springbootproject;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +15,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${name}")
+    private String name;
+
+    @Value("${age}")
+    private Integer age;
+
+    @Value("${content}")
+    private String content;
+
+    @Autowired
+    private PeopleProperties peopleProperties;
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String say() {
-        return "Hello Spring Boot";
+        return "Hello Spring Boot !";
+    }
+
+    @RequestMapping(value = "/info", method = RequestMethod.GET)
+    public String myInfo() {
+        return "My Name is" + name + ", I am " + age + " years old.";
+    }
+
+    @RequestMapping(value = "/content", method = RequestMethod.GET)
+    public String content() {
+        return content;
+    }
+
+    @RequestMapping(value = "/people")
+    public String peopleProperties() {
+        return peopleProperties.getName();
     }
 }
